e6cc2bbaccc644f61bdc91207e63c918d991562d
Tyler Savery <tyler@theyoungastronauts.com>
2022-08-30 18:34:48 -0400

    update

--- next.config.js
+++ next.config.js
@@ -1,15 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextTranslate = require('next-translate')
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
 
-const nextConfig = {
+const nextConfig =  {
   reactStrictMode: true,
   swcMinify: true,
   i18n: {
     locales: ['en', 'es'],
     defaultLocale: 'en',
-    
   }
 }
 
-module.exports = nextConfig
+module.exports =  nextConfig
 module.exports = nextTranslate()
+module.exports = withPlugins([
+  [optimizedImages, {
+    /* config for next-optimized-images */
+  }],
+
+  // your other plugins here
+
+]);
