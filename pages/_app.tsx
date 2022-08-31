c0c13894e036c0007ef151e7c8729aa206fcff0f
Tyler Savery <tyler@theyoungastronauts.com>
2022-08-31 13:06:51 -0400

    favicon and analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,28 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-9WC0BDHRBK"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-9WC0BDHRBK');
+        `}
+      </Script>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp
