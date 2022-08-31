49993a7b41b6316da78e3d25675ecd21656ed8d2
Tyler Savery <tyler@theyoungastronauts.com>
2022-08-30 20:47:22 -0400

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next'
+import { NextSeo } from 'next-seo'
 import Head from 'next/head'
 import Image from 'next/image'
 import AboutSection from '../components/about-section'
@@ -10,18 +11,45 @@ import TopSection from '../components/top-section'
 import styles from '../styles/Home.module.scss'
 
 const Home: NextPage = () => {
+  const title = "Storyplace";
+  const description = "Storyplace is an empathy-driven social platform where you can share and read stories from people around the world.";
+
   return (
-    <main>
+    <>
+      <NextSeo
+        title={title}
+        description={description}
+        canonical="https://new.storyplace.com"
+        openGraph={{
+          url: "https://new.storyplace.com",
+          title: title,
+          description: description,
+          images: [{
+            url: "https://storyplace-cdn.s3.amazonaws.com/asset/image/4adca168-211c-43bd-ad55-890de96452f1/og.jpg",
+            width: 1640,
+            height: 924,
+            alt: 'Storyplace',
+            type: 'image/jpeg',
+          }],
+          site_name: "Storyplace",
 
-      <Header />
-      <TopSection />
-      <AboutSection />
-      <OurStorySection />
-      <AmbassadorSection />
-      <Footer />
+        }}
+        twitter={{
+          handle: "@mystoryplace"
+        }}
+      />
+      <main>
+
+        <Header />
+        <TopSection />
+        <AboutSection />
+        <OurStorySection />
+        <AmbassadorSection />
+        <Footer />
 
 
-    </main>
+      </main>
+    </>
   )
 }
 
