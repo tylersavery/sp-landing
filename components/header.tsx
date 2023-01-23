4cd0616ebabb228ea61682aae07c5141183ba431
TeniDev <nunezg.john@gmail.com>
2023-01-23 14:23:44 -0500

    header fix

--- components/header.tsx
+++ components/header.tsx
@@ -20,37 +20,39 @@ const Header = () => {
         window.Intercom('show');
     }
 
-    return <header className={`${styles.header} ${offset > 100 ? styles.header__opaque : ''}`}>
+    return <header className={`${styles.header} ${offset > 80 ? styles.header__opaque : ''}`}>
 
 
         <div className="container">
 
             <div className="d-flex p-2 justify-content-between align-items-center">
-                <Link href="/">
-                    <Image src={"/images/logo.svg"} width={260} height={80} alt="Storyplace" />
-                </Link>
+                <div className="d-flex align-items-center">
+                    <Link href="/">
+                        <Image src={"/images/logo.svg"} width={200} height={60} alt="Storyplace" />
+                    </Link>
+                    <div className="mx-4"></div>
+                    <div className="mx-4">
+                        <Link href="https://app.storyplace.com/app/home" target={"_blank"}><a className={styles.link}>{t('Home')}</a></Link>
+                    </div>
+                    <div className="mx-4">
+                        <Link href="https://app.storyplace.com/app/explore" target={"_blank"}><a className={styles.link}>{t('Explore')}</a></Link>
+                    </div>
+                    <div className="mx-4">
+                        <Link href="https://app.storyplace.com/app/circles" target={"_blank"}><a className={styles.link}>{t('Circles')}</a></Link>
+                    </div>
+                </div>
 
                 <div className="d-flex align-items-center">
-                    {/* <div className="mx-4">
-                        <Link href="/"><a className={styles.link}>{t('About')}</a></Link>
-                    </div> */}
                     <div className="mx-4">
-                        <Link href="/"><a className={styles.link} onClick={() => handleHelp()}>{t('Help')}</a></Link>
-                    </div>
-
-                    <div className="mx-4">
-                        <div className={styles.button}>
-                            <a href="https://old.storyplace.com/get-app" target={"_blank"} className={styles.link} rel="noreferrer">{t('Download App')}</a>
-                        </div>
-                    </div>
-
-                    <div className="mx-4">
-                        <Link href="https://app.storyplace.com/" target={"_blank"}><a className={`${styles.link} ${styles.link__small}`}>{t('Use web version')}</a></Link>
-                    </div>
-                    <div className="mx-4">
-                        <Link href="/" locale="english"><a className={`${styles.link} ${styles.link__small}`}>English</a></Link>
+                        <Link href="/" locale="english"><a className={`${styles.link} ${styles.link__small}`}>EN</a></Link>
                         <span className={`${styles.link} ${styles.link__small} ${styles.link__light}`}> | </span>
-                        <Link href="/" locale="espanol"><a className={`${styles.link} ${styles.link__small}`}>Español</a></Link>
+                        <Link href="/" locale="espanol"><a className={`${styles.link} ${styles.link__small}`}>ES</a></Link>
+                    </div>
+                    <div className="mx-4">
+                        <Link href="https://app.storyplace.com/auth/register" target={"_blank"}><a className={styles.link}>{t('Sign up')}</a></Link>
+                    </div>
+                    <div className="mx-4">
+                        <Link href="https://app.storyplace.com/auth/login" target={"_blank"}><a className={styles.link}>{t('Login')}</a></Link>
                     </div>
                 </div>
             </div>
