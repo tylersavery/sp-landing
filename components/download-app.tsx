30fbf27e74ce2286efde81b21dc81e38130eb651
TeniDev <nunezg.john@gmail.com>
2023-02-14 17:22:27 -0500

    Menu for mobile | label for web version in mobile

--- components/download-app.tsx
+++ components/download-app.tsx
@@ -1,3 +1,4 @@
+import useTranslation from "next-translate/useTranslation";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -8,21 +9,29 @@ interface Props {
 
 export const DownloadApp = (props: Props) => {
 
-    return <div className={`d-flex ${props.leftAligned ? '' : 'justify-content-center'} align-items-center`}>
-        <div className="me-2">
-            <a href="https://apps.apple.com/app/storyplace/id1289523039" target="_blank" rel="noopener noreferrer">
-                <Image src="/images/ios.png" width={202} height={60} alt="Download on iOS App Store" />
-            </a>
-        </div>
-        <div>
-            <a href="https://play.google.com/store/apps/details?id=com.storyplace" target="_blank" rel="noopener noreferrer">
-                <Image src="/images/android.png" width={202} height={60} alt="Download on Google Play" />
-            </a>
-        </div>
+    const { t } = useTranslation('common')
+
+    return <div className="d-flex flex-column">
+            <div className={`d-flex ${props.leftAligned ? '' : 'justify-content-center'} align-items-center`}>
+                <div className="me-2">
+                    <a href="https://apps.apple.com/app/storyplace/id1289523039" target="_blank" rel="noopener noreferrer">
+                        <Image src="/images/ios.png" width={202} height={60} alt="Download on iOS App Store" />
+                    </a>
+                </div>
+                <div>
+                    <a href="https://play.google.com/store/apps/details?id=com.storyplace" target="_blank" rel="noopener noreferrer">
+                        <Image src="/images/android.png" width={202} height={60} alt="Download on Google Play" />
+                    </a>
+                </div>
+                {props.withWeb ?
+                    <div className="ms-3">
+                        <a href="https://app.storyplace.com/app/home" className="text-white" style={{ fontWeight: 600, textDecoration: 'none' }}>{t('Use web version')}</a>
+                    </div> : null}
+            </div>
         {props.withWeb ?
-            <div className="ms-3">
-                <a href="https://app.storyplace.com/app/home" className="text-white" style={{ fontWeight: 600, textDecoration: 'none' }}>Use Web Version</a>
-            </div> : null}
+                null : <div className="d-flex p-3 justify-content-center">
+                    <a href="https://app.storyplace.com/app/home" className="text-white" style={{ fontWeight: 600, textDecoration: 'none' }}>{t('Use web version')}</a>
+                </div>}
     </div>
 }
 
