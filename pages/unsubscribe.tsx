e70375002ce176dfbd1464c43a78489942b736f8
Tyler Savery <tyler@theyoungastronauts.com>
2023-05-30 12:41:01 -0400

    update

---
+++ pages/unsubscribe.tsx
@@ -0,0 +1,21 @@
+/* eslint-disable react/no-unescaped-entities */
+import { NextPage } from "next"
+import LegalContainer from "../components/legal-container";
+
+const Terms: NextPage = () => {
+
+    return (
+        <>
+            <LegalContainer>
+
+                <h2 className="text-center">Unsubscribe from Email Updates</h2>
+
+            </LegalContainer>
+
+        </>
+
+    )
+}
+
+
+export default Terms;
