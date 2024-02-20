394b53b04c671fda1952d6aee87dea20d6f2abff
Tyler Savery <tyler@theyoungastronauts.com>
2024-02-20 15:18:45 -0500

    google tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,14 @@ import 'bootstrap/dist/css/bootstrap.css'
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 import Script from 'next/script';
-
+import { GoogleTagManager } from '@next/third-parties/google'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
+      <GoogleTagManager gtmId="G-D7CJXYN6N8" />
+
+
     </>
   );
 }
